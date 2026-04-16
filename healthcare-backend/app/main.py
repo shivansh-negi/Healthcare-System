09816f3aa8720100e980a0d62a5e24da9d3f9c19
shivansh-negi <shivanshnegi1219@gmail.com>
2026-04-16 11:41:08 +0530

    Your message about what you changed

--- healthcare-backend/app/main.py
+++ healthcare-backend/app/main.py
@@ -58,7 +58,18 @@ app = FastAPI(
 # ---- CORS Middleware ----
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins_list,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "http://localhost:5175",
+        "http://localhost:5176",
+        "http://localhost:3000",
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:5174",
+        "http://127.0.0.1:5175",
+        "http://127.0.0.1:3000",
+        *settings.cors_origins_list,
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
